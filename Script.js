45e483e8ec873f80a1bab4e2d412c7cff647c2a1
imaduddin-faalih <imaduddin.faalih@gmail.com>
2025-01-02 16:52:43 +0000

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,6 +1,1 @@
-// Optional: Add an interaction effect or custom analytics
-document.querySelectorAll('.btn').forEach(button => {
-    button.addEventListener('click', () => {
-        alert(`Redirecting to ${button.textContent}...`);
-    });
-});
+
