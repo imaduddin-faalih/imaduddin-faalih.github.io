90e502d543b0567268a6b1004509d368b649dc3e
imaduddin-faalih <imaduddin.faalih@gmail.com>
2025-01-08 21:54:03 +0000

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,14 @@
+// Script sederhana: toggle menu di mobile
+document.addEventListener("DOMContentLoaded", function () {
+  const navToggle = document.getElementById("navToggle");
+  const navMenu = document.getElementById("navMenu");
+  const navRight = document.getElementById("navRight");
+
+  navToggle.addEventListener("click", () => {
+    // Tampilkan / sembunyikan menu & tombol di mobile
+    const isVisible = navMenu.style.display === "flex";
+    navMenu.style.display = isVisible ? "none" : "flex";
+    navRight.style.display = isVisible ? "none" : "flex";
+  });
+});
 
